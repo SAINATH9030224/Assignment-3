b7ce0e713b254209ccdc4a75b78b7ab43f138128
SAINATH9030224 <guddetir@usf.edu>
2024-09-15 23:47:56 -0400

    Add files via upload

---
+++ assisgnment3.R
@@ -0,0 +1,23 @@
+Name <- c("Jeb", “Donald”, "Ted”, “Marco” “Carly”, “Hillary”, “Berine”)
+ABC political poll results <- c(4, 62 51, 21, 2, 14, 15)
+
+CBS political poll results <- c(12, 75, 43, 19, 1, 21, 19)
+
+
+#Fixing the code
+
+
+
+Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+
+ABC_political_poll_results <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS_political_poll_results <- c(12, 75, 43, 19, 1, 21, 19)
+
+
+
+my_data <- data.frame ( Names = c("Jeb", "Donald", "Ted", "Marco","Carly", "Hillary", "Berine"),
+                       Abc_poll = c(4, 62, 51, 21, 2, 14, 15),
+                       CBS_poll = c(12, 75, 43, 19, 1, 21, 19))
+                       
+my_data
